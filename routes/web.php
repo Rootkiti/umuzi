1dcdb9c7d2b1aa2c9cead46d561107802be1d307
rootkiti <robertu20002000@gmail.com>
2024-11-11 07:50:55 +0200

    defining costom routes

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::view('/example-page', 'example-page');
 Route::view('/example-auth', 'example-auth');
-Route::get('/', function () {
-    return view('example-page');
-});
